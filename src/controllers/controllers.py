70a2bb0c14e171730c902145b7bd6bcd805e78cf
imrooteodoro <adelsonteodoro67@gmail.com>
2025-01-04 15:46:47 -0300

    feat: adiciona os controladores

---
+++ src/controllers/controllers.py
@@ -0,0 +1,16 @@
+import re
+import os
+import subprocess
+from flask import logging
+from services.gemini import BotService
+from services.generate import generate_latex_with_ai
+from system_context.context import system_context
+
+
+def generate_content(user_prompt):
+    final_content = BotService.connect_agent(system_context(), user_prompt)
+    return final_content
+
+def generate_latex_ai(app):
+    return generate_latex_with_ai(app)
+    
